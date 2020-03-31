27dadf98ef4479ce2fd5af7dd235c00267c0dce8
Glenn <glenn-edgar@onyxengr.com>
2020-03-31 16:43:49 -0700

    daily commit

--- code/master_pod_initialization_py3.py
+++ code/master_pod_initialization_py3.py
@@ -1,6 +1,9 @@
 import redis
 import json
+import time
 from pod_control.docker_interface_py3 import Docker_Interface
+from redis_support_py3.graph_query_support_py3 import  Query_Support
+
 
 redis_site_file = "/mnt/ssd/site_config/redis_server.json"
 password_file   = "passwords.py"
@@ -8,6 +11,10 @@ password_file   = "passwords.py"
 predefined_containers= {}
 predefined_containers["redis"] =["redis",'/home/pi/pod_control/code/startup_scripts/redis_run.bsh']
 predefined_containers["postgres"] =["postgres",'/home/pi/pod_control/code/startup_scripts/postgres_run.bsh']
+predefined_containers["pod_construct_graph"] =["pod_utility_function",'/home/pi/pod_control/code/startup_scripts/pod_util_construct_graph.bsh']
+predefined_containers["pod_util_set_passwords"] =["pod_utility_function",'/home/pi/pod_control/code/startup_scripts/pod_util_set_passwords.bsh']
+predefined_containers["pod_util_echo"] =["pod_utility_function",'/home/pi/pod_control/code/startup_scripts/pod_util_echo.bsh']
+predefined_containers["monitor_redis"] =["monitor_redis",'/home/pi/pod_control/code/startup_scripts/redis_monitoring.bsh']
 
 
 def start_postgres_container():
@@ -20,9 +27,12 @@ def start_redis_container():
     docker_control.container_up(data[0],data[1])
     
 def start_utility_pod():
-    pass
-    
-    
+    data = predefined_containers["pod_construct_graph"]
+    docker_control.container_up(data[0],data[1])
+    data = predefined_containers["pod_util_set_passwords"]
+    docker_control.container_up(data[0],data[1])   
+    data = predefined_containers["pod_util_echo"]
+    docker_control.container_up(data[0],data[1])    
 def run_utility_functions():
     pass
     
@@ -33,13 +43,25 @@ def wait_for_redis_db(site_data):
             redis_handle = redis.StrictRedis( host = site_data["host"] , port=site_data["port"], db=site_data["graph_db"])
             return
         except:
+           time.sleep(10)
            pass
 
-def start_master_pod_controller():
-    pass
 
-def start_slave_pod_container():
-    pass
+
+def start_slave_pod_container(site_data):
+   qs = Query_Support( site_data )
+   query_list = []
+   query_list = qs.add_match_relationship( query_list,relationship="SITE",label=site_data["site"] )
+   query_list = qs.add_match_terminal( query_list,relationship="PROCESSOR",label=site_data["local_node"] )
+   processor_sets, processor_nodes = qs.match_list(query_list)
+   containers = processor_nodes[0]["containers"]
+   print("containers",containers)
+   for i in containers:
+       data = predefined_containers[i]
+       docker_control.container_up(data[0],data[1])
+
+
+
 
 
 docker_control = Docker_Interface()
@@ -61,13 +83,13 @@ if site_data["master"]:
    start_redis_container()
    start_utility_pod()
    run_utility_functions()
-   start_master_pod_controller()
+   #start_master_pod_controller()
    
 else:
   wait_for_redis_db(site_data)
 
 
-start_slave_pod_container()
+start_slave_pod_container(site_data)
 
 
 
