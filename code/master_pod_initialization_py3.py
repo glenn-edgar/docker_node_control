038cbed0475a70eb7a214ad92b45d723c572e43e
Glenn <glenn-edgar@onyxengr.com>
2020-04-01 16:04:37 -0700

    daily commit

--- code/master_pod_initialization_py3.py
+++ code/master_pod_initialization_py3.py
@@ -15,12 +15,15 @@ predefined_containers["pod_construct_graph"] =["pod_utility_function",'/home/pi/
 predefined_containers["pod_util_set_passwords"] =["pod_utility_function",'/home/pi/pod_control/code/startup_scripts/pod_util_set_passwords.bsh']
 predefined_containers["pod_util_echo"] =["pod_utility_function",'/home/pi/pod_control/code/startup_scripts/pod_util_echo.bsh']
 predefined_containers["monitor_redis"] =["monitor_redis",'/home/pi/pod_control/code/startup_scripts/redis_monitoring.bsh']
-
+predefined_containers["ethereum"] =["ethereum_go",'/home/pi/pod_control/code/startup_scripts/ethereum_run.bsh']
 
 def start_postgres_container():
     data = predefined_containers["postgres"]
     docker_control.container_up(data[0],data[1])
-
+    
+def start_ethereum():
+    data = predefined_containers["ethereum"]
+    docker_control.container_up(data[0],data[1])
 
 def start_redis_container():
     data = predefined_containers["redis"]
@@ -79,6 +82,7 @@ if "master" not in site_data:
                   
 
 if site_data["master"]:
+   start_ethereum()
    start_postgres_container()
    start_redis_container()
    start_utility_pod()
